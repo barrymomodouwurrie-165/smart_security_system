ac336d7e4fc88140e49f8ef6afb230f463a1e71b
momodou165 <barrymomodouwurry165@gmail.com>
2026-06-04 22:34:55 +0000

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Dashboard from "./Pages/Dashboard";
+import "./App.css";
+
+function App() {
+  return (
+    <div data-theme="forest">
+      <Dashboard />;
+    </div>
+  );
+}
+
+export default App;
