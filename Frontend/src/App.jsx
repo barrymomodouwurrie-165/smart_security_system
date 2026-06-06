464ac8a8669d036b01f3fdb886e9f60598962acf
momodou165 <barrymomodouwurry165@gmail.com>
2026-06-06 02:11:56 +0000

    Adjusted the style of the background to be fixed on scroll.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="min-h-screen w-full h-full">
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_65%,#63e_110%)]"></div>
+      <div class="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#63e_120%)]"></div>
       <Dashboard />;
     </div>
   );
